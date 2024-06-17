ccc429b42b7051d25c381071a43f4565f6fc02bf
adilzhanshukenov <shukenovadilzhan@gmail.com>
2024-06-17 23:13:32 +0500

    added categories

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,50 @@ function App() {
   const [start, setStart] = useState(false)
   const [data, setData] = useState([])
   const [reset, setReset] = useState(false)
-  const [isShowAnswers, setIsShowAnswers] = useState(false)
+  const [isShowAnswers, setIsShowAnswers] = useState(false)    
+  const [formData, setFormData] = useState(
+    {
+        amountOfQuestions: '5',
+        category: '',
+        difficulty: ''
+    }
+  )
+
+  const {amountOfQuestions, category, difficulty} = formData
 
   useEffect(() => {
 
-    fetch("https://opentdb.com/api.php?amount=5&category=15&type=multiple")
-    .then(res => res.json())
-    .then(data => {
-        setData(() => {
-            return data.results.map(question => {
-                
-                const incorrect = question.incorrect_answers.map(answer => {
-                    return {value: answer, id: nanoid(), isHeld: false, isCorrect: false}
-                });
 
-                const correct = {value: question.correct_answer, id: nanoid(), isHeld: false, isCorrect: true}
+    const fetchData = async () => {
+      try {
+          setTimeout(async () => {
+            const response = await fetch(`https://opentdb.com/api.php?amount=${amountOfQuestions}&category=${category}&type=multiple&difficulty=${difficulty}`);
+            const result = await response.json();
+            setData(() => {
+              return result.results.map(question => {
+                  
+                  const incorrect = question.incorrect_answers.map(answer => {
+                      return {value: answer, id: nanoid(), isHeld: false, isCorrect: false}
+                  });
+  
+                  const correct = {value: question.correct_answer, id: nanoid(), isHeld: false, isCorrect: true}
+  
+                  const randomIndex = Math.floor(Math.random() * (incorrect.length + 1));
+                  const allAnswersArr = [...incorrect];
+                  allAnswersArr.splice(randomIndex, 0, correct);
+                  
+                  
+                  return {...question, allAnswers: allAnswersArr, id:nanoid()};
+              })
+          });
+          }, 1000)
+      } catch(error) {
+          console.error("You catched error", error);
 
-                const randomIndex = Math.floor(Math.random() * (incorrect.length + 1));
-                const allAnswersArr = [...incorrect];
-                allAnswersArr.splice(randomIndex, 0, correct);
-                
-                
-                return {...question, allAnswers: allAnswersArr, id:nanoid()};
-            })
-        })
-    })
-  }, [reset])
+      }
+    }
+    fetchData();
+  }, [reset, amountOfQuestions, category, difficulty])
 
   function updateHeld(qId, aId){
     setData(prevData => {
@@ -60,11 +78,20 @@ function App() {
 
   function startQuiz() {
     setStart(prevState => !prevState)
+
   }
 
   function resetGame() {
     setIsShowAnswers(false)
     setReset(prev => !prev)
+}
+
+function handleChange(event) {
+  const {name, value} = event.target
+  setFormData(prevFormData => {
+      return {...prevFormData, 
+              [name]: value}
+  })
 }
 
 let score = 0;
@@ -83,6 +110,8 @@ if(isShowAnswers) {
         <Start 
           key={nanoid()}
           click={() => startQuiz()}
+          handleChange={() => handleChange}
+          formData={formData}
         />
       :
         <Quiz key={nanoid()}
